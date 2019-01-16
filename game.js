ca92b9b08edc5d925809a05288e99c65bcc77267
kgorlo <46658929+kgorlo@users.noreply.github.com>
2019-01-16 21:21:44 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ const gracz1 = document.getElementById('gracz1')
 const gracz2 = document.getElementById('gracz2')
 const welcome = document.querySelector(".welcome");
 const gray = document.querySelector(".gray");
-
-
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const player1 = document.querySelector(".gracz1 h2");
@@ -27,71 +25,57 @@ ok.addEventListener('click', function () {
     player2.textContent = gracz2.value;
     if (gracz1.value == "" || gracz2.value == "") {
         alert("Nie podano imienia gracza!");
+    } else if (gracz1.value === gracz2.value) {
+        alert("Nazwy graczy są takie same!");
     } else {
         welcome.classList.add("hidden");
         gray.classList.add("hidden");
     }
 })
 
+const rzut1 = function (player) {
 
-
-
-
-const rzut1 = function () {
+    console.log(player);
     players++;
-    const dice1 = Math.floor(Math.random() * 6 + 1)
-    const dice2 = Math.floor(Math.random() * 6 + 1)
-    const dice3 = Math.floor(Math.random() * 6 + 1)
-    const dice4 = Math.floor(Math.random() * 6 + 1)
-    const dice5 = Math.floor(Math.random() * 6 + 1)
-    const rzut = [dice1, dice2, dice3, dice4, dice5]
+    const losowanie = [1, 1, 1, 1, 1];
+    const rzut = losowanie.map((x) => {
+        return (x * (Math.floor(Math.random() * 6 + 1)))
+    });
+
     console.log(rzut)
 
-    for (i = 1; i < 6; i++) {
-        const kostka = document.createElement("div");
-        dices1.appendChild(kostka);
-        kostka.innerHTML = `<div class='dice1_${i}'><img src='dice${rzut[i-1]}.png'></div>`;
-        let dice = document.querySelector(".dice1_" + i);
-        dice.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
-    }
-    player1score = dice1 + dice2 + dice3 + dice4 + dice5
-    pkt1.textContent = player1score;
-    btn1.removeEventListener("click", rzut1);
-    if (players == 2) {
-        star.classList.remove("hidden");
-        btnnew.classList.remove("hidden");
+    if (player == 1) {
 
-        if (player1score > player2score) {
-            document.querySelector(".wynik p").innerHTML = `Wygrał gracz: ${player1.textContent} ${player1score} vs ${player2score}`
-        } else if (player2score > player1score) {
-            document.querySelector(".wynik p").innerHTML = `Wygrał gracz: ${player2.textContent} ${player2score} vs ${player1score}`
-        } else if (player2score == player1score) {
-            document.querySelector(".wynik p").innerHTML = `REMIS ${player2score} vs ${player1score}`
+        for (i = 1; i < 6; i++) {
+            const kostka = document.createElement("div");
+            dices1.appendChild(kostka);
+            kostka.innerHTML = `<div class='dice${player}_${i}'><img src='dice${rzut[i-1]}.png'></div>`;
+            let dice = document.querySelector(`.dice${player}_` + i);
+            dice.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
         }
+
+        player1score = rzut.reduce(function (prev, next) {
+            return prev + next;
+        });
+
+        pkt1.textContent = player1score;
+        btn1.removeEventListener("click", rzut1);
+
+    } else if (player == 2) {
+        for (i = 1; i < 6; i++) {
+            const kostka = document.createElement("div");
+            dices2.appendChild(kostka);
+            kostka.innerHTML = `<div class='dice${player}_${i}'><img src='dice${rzut[i-1]}.png'></div>`;
+            let dice = document.querySelector(`.dice${player}_` + i);
+            dice.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
+        }
+        player2score = rzut.reduce(function (prev, next) {
+            return prev + next;
+        });
+
+        pkt2.textContent = player2score;
+        btn2.removeEventListener("click", rzut1);
     }
-
-}
-
-const rzut2 = function () {
-    players++;
-    const dice1 = Math.floor(Math.random() * 6 + 1)
-    const dice2 = Math.floor(Math.random() * 6 + 1)
-    const dice3 = Math.floor(Math.random() * 6 + 1)
-    const dice4 = Math.floor(Math.random() * 6 + 1)
-    const dice5 = Math.floor(Math.random() * 6 + 1)
-    const rzut = [dice1, dice2, dice3, dice4, dice5]
-    console.log(rzut)
-
-    for (i = 1; i < 6; i++) {
-        const kostka = document.createElement("div");
-        dices2.appendChild(kostka);
-        kostka.innerHTML = `<div class='dice2_${i}'><img src='dice${rzut[i-1]}.png'></div>`;
-        let dice = document.querySelector(".dice2_" + i);
-        dice.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
-    }
-    player2score = dice1 + dice2 + dice3 + dice4 + dice5;
-    pkt2.textContent = player2score;
-    btn2.removeEventListener("click", rzut2);
     if (players == 2) {
         star.classList.remove("hidden");
         btnnew.classList.remove("hidden");
@@ -106,13 +90,13 @@ const rzut2 = function () {
     }
 }
 
-
-btn1.addEventListener("click", rzut1);
-btn2.addEventListener("click", rzut2);
+const element = "";
+btn1.addEventListener("click", rzut1.bind(element, 1));
+btn2.addEventListener("click", rzut1.bind(element, 2));
 
 star.addEventListener("click", function () {
     btn1.addEventListener("click", rzut1);
-    btn2.addEventListener("click", rzut2);
+    btn2.addEventListener("click", rzut1);
     players = 0;
     player1score = 0;
     player2score = 0;
